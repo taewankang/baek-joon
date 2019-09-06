0a433fdf7d242775886f6963619e8982748864ea
taewankang <king6096211@naver.com>
2019-09-06 23:13:01 +0900

    2019.09

--- 2019.09/1937.cpp
+++ 2019.09/1937.cpp
@@ -6,27 +6,54 @@ int N;
 int x_arr[4] = { 1, -1, 0, 0 };
 int y_arr[4] = { 0, 0, 1, -1 };
 int maximum;
+
+
 vector<vector<int>> vec(501);
 int way[501][501];
-void dfs(int start_y, int stat_x, int next_y, int next_x) {
-/*
-	만약 way[next_y][next_x]의 값이 -1이라면 한번도 안 다녀온 것
-	계산해서 집어넣기
-	한번씩 돌면서 way에 -1값이 없을때까지 만들어주기
-*/
+int startX, startY;
+void show() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cout << way[i][j] << " ";
+		cout << endl;
+	}
+}
+void dfs(int start_y, int start_x, int next_y, int next_x, int depth) {
+	cout << endl;
+	show();
+	bool no_way = false;
+	for (int i = 0; i < 4; i++) {
+		next_x = x_arr[i] + start_x;
+		next_y = y_arr[i] + start_y;
+		cout << "before start_x: " << start_x << " start_y: " << start_y << " next_x: " << next_x << " next_y " << next_y << endl;
+		if (next_x > 0 && next_x <= N && next_y > 0 && next_y <= N && vec[start_y][start_x] < vec[next_y][next_x]) {
+			cout << "start_x: " << start_x << " start_y: " << start_y << " next_x: " << next_x << " next_y " << next_y << endl;
+			no_way = true;
+			if (way[next_y][next_x] == -1) {
+				dfs(next_y, next_x, 0, 0, depth + 1);
+			}
+			else {
+				if (way[start_y][start_x] < way[next_y][next_x]) {
+					way[start_y][start_x] = way[next_y][next_x] + 1;
+				}
+			}
+		}
+	}
+	if (no_way == false)
+		way[start_y][start_x] = 1;
 }
 
 void start() {
-	for (int i = 1; i < 501; i++)
-		for (int j = 1; j < 501; j++)
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
 			way[i][j] = -1;
 }
 
 int main(void) {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	start();
 	cin >> N;
+	start();
 	int n;
 	for (int i = 1; i <= N; i++) {
 		vec[i].push_back(0);
@@ -38,7 +65,10 @@ int main(void) {
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			dfs(i, j, 0, 0);
+			startX = j;	startY = i;
+			if (way[startY][startX] == -1)
+				way[startY][startX] = 1;
+			dfs(i, j, 0, 0, 1);
 		}
 	}
 	cout << maximum << endl;
