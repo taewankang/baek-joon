e9dee630e717ac3cfb348b03308b5ef0e059d460
taewankang <king6096211@naver.com>
2019-09-06 23:22:23 +0900

    2019.09

--- 2019.09/1937.cpp
+++ 2019.09/1937.cpp
@@ -6,37 +6,30 @@ int N;
 int x_arr[4] = { 1, -1, 0, 0 };
 int y_arr[4] = { 0, 0, 1, -1 };
 int maximum;
-
-
 vector<vector<int>> vec(501);
 int way[501][501];
 int startX, startY;
 void show() {
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++)
-			cout << way[i][j] << " ";
-		cout << endl;
+			if (maximum < way[i][j])
+				maximum = way[i][j];
 	}
 }
+
 void dfs(int start_y, int start_x, int next_y, int next_x, int depth) {
-	cout << endl;
-	show();
 	bool no_way = false;
 	for (int i = 0; i < 4; i++) {
 		next_x = x_arr[i] + start_x;
 		next_y = y_arr[i] + start_y;
-		cout << "before start_x: " << start_x << " start_y: " << start_y << " next_x: " << next_x << " next_y " << next_y << endl;
 		if (next_x > 0 && next_x <= N && next_y > 0 && next_y <= N && vec[start_y][start_x] < vec[next_y][next_x]) {
-			cout << "start_x: " << start_x << " start_y: " << start_y << " next_x: " << next_x << " next_y " << next_y << endl;
 			no_way = true;
-			if (way[next_y][next_x] == -1) {
+			if (way[next_y][next_x] == -1)
 				dfs(next_y, next_x, 0, 0, depth + 1);
+			if (way[start_y][start_x] <= way[next_y][next_x]) {
+				way[start_y][start_x] = way[next_y][next_x] + 1;
 			}
-			else {
-				if (way[start_y][start_x] < way[next_y][next_x]) {
-					way[start_y][start_x] = way[next_y][next_x] + 1;
-				}
-			}
+
 		}
 	}
 	if (no_way == false)
@@ -71,6 +64,7 @@ int main(void) {
 			dfs(i, j, 0, 0, 1);
 		}
 	}
+	show();
 	cout << maximum << endl;
 	system("pause");
 	return 0;
