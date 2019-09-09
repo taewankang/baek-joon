370fd6d4d7262dd2c2fe6ed3378f57c64d911282
taewankang <king6096211@naver.com>
2019-09-09 10:50:03 +0900

    2019.09

--- 2019.09/1405.cpp
+++ 2019.09/1405.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int n;
 double E, W, S, N;
+double total = 0;
+bool check[30][30] = { false, };
+void backtracking(int depth) {
+	/*
+		백트래킹을 이용해서 방문하지 않은 곳을 찾아서 체크
+	*/
+}
 
 int main(void) {
 	cin.tie(NULL);
@@ -10,7 +17,8 @@ int main(void) {
 	cout << fixed;
 	cin >> n >> E >> W >> S >> N;
 	E /= 100;	W /= 100;	S /= 100;	N /= 100;
-
+	cout << E << " " << W << " " << N << " " << S << endl;
+	backtracking();
 
 	system("pause");
 	return 0;
