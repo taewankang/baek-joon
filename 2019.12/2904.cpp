cc2048e27709bf3ede8f9fa927020f926c35833a
taewankang <king6096211@naver.com>
2019-12-31 16:32:43 +0900

    2019.12

--- 2019.12/2904.cpp
+++ 2019.12/2904.cpp
@@ -1,110 +1,80 @@
+/*
+2019.12.31
+1. prime() 함수를 이용해서 소수로만 이루어진 배열을 생성했다.
+2. 값을 입력 받으면 devide함수를 이용해서 소인수분해한 결과를 저장해준다.
+3. 가장 큰 점수는 pow(해당 소수가 사용된 개수 / N)을 곱하면서 구했다.
+4. 최소 몇 번만에 점수를 얻는지는 해당 소수가 사용된 평균 - 소수가 쓰인 횟수만큼을 더하면서 구했다.
+*/
+
 #include<iostream>
 #include<vector>
-#include<queue>
+#include<math.h>
 using namespace std;
-vector<pair<int, int>> virus_location;
-int empty_num = 0, virus_num = 0;
-int N, M, n, result = 987654321;
-vector<bool> selected;
-int arr_x[4] = { 1, -1, 0, 0 };
-int arr_y[4] = { 0, 0, 1, -1 };
-int all_case = 0, fail_case = 0, activated = 0;
-
-void bfs(vector<vector<int>> vec) {
-	queue<pair<int, int>> q;
-	int counting = 0;
-	int minimum = 0;
-	for (int i = 0; i < selected.size(); i++) {
-		if (selected[i] == true) {
-			q.push({ virus_location[i + 1].first, virus_location[i + 1].second });
-			vec[virus_location[i + 1].first][virus_location[i + 1].second] = 1;
-		}
+vector<int> prime_vec;
+vector<int> prime() {
+	vector<int> vec(1000001), vec2;
+	for (int i = 2; i <= 1000000; i++)
+		vec[i] = i;
+	for (int i = 2; i <= sqrt(1000000); i++) {
+		if (vec[i] == 0)
+			continue;
+		for (int j = 2; i * j <= 1000000; j++)
+			if (vec[i * j] != 0)
+				vec[i * j] = 0;
 	}
 
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int next_y = y + arr_y[i];
-			int next_x = x + arr_x[i];
-			if (empty_num == counting) {
-				if (result > minimum)
-					result = minimum;
-				return;
-			}
-
-			if (next_x > 0 && next_x <= N && next_y > 0 && next_y <= N && (vec[next_y][next_x] == 0 || vec[next_y][next_x] == -2)) {
-				if (vec[next_y][next_x] == -2)
-					counting--;
-				q.push({ next_y, next_x });
-				vec[next_y][next_x] = vec[y][x] + 1;
-				if (vec[next_y][next_x] > minimum)
-					minimum = vec[next_y][next_x];
-				counting++;
-			}
-		}
-		if (empty_num == counting) {
-			if (result > minimum)
-				result = minimum;
-		}
-	}
-	if (empty_num != counting)
-		fail_case++;
-
+	for (int i = 2; i <= 1000000; i++)
+		if (vec[i] != 0)
+			vec2.push_back(vec[i]);
+	return vec2;
 }
 
-void virus_select(vector<vector<int>>& vec, int idx, int cnt) {
-	if (M == cnt) {
-		bfs(vec);
-		all_case++;
-		return;
+vector<int> devide(int N) {
+	vector<int> vec(prime_vec.size());
+	while (N != 1) {
+		for (int i = 0; i < prime_vec.size(); i++) {
+			if (N % prime_vec[i] == 0) {
+				vec[i]++;
+				N /= prime_vec[i];
+				break;
+			}
+		}
 	}
-	for (int i = idx; i < virus_num; i++) {
-		if (selected[i] == true)	continue;
-		selected[i] = true;
-		virus_select(vec, i, cnt + 1);
-		selected[i] = false;
-	}
+	return vec;
 }
 
 int main(void) {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-	cin >> N >> M;
-	vector<vector<int>> vec(N + 1);
-	virus_location.push_back({ 0, 0 });
-	for (int i = 1; i <= N; i++) {
-		vec[i].push_back(0);
-		for (int j = 1; j <= N; j++) {
-			cin >> n;
-			if (n == 2) {
-				virus_num++;
-				virus_location.push_back({ i, j });
-				selected.push_back(false);
-				vec[i].push_back(-2);
-				continue;
+	int N, n;
+	long long aver = 1;
+	int counting = 0;
+	cin >> N;
+	vector<int> vec;
+	vector<vector<int>> all_vec(N);
+	prime_vec = prime();
+
+	for (int i = 0; i < N; i++) {
+		cin >> n;
+		all_vec[i] = devide(n);
+	}
+
+	for (int i = 0; i < prime_vec.size(); i++) {
+		int sum = 0;
+		for (int j = 0; j < N; j++) {
+			sum += all_vec[j][i];
+		}
+		if (sum / N != 0) {
+			int aver2 = sum / N;
+			aver *= pow(prime_vec[i], aver2);
+			for (int j = 0; j < N; j++) {
+				if (all_vec[j][i] < aver2)
+					counting += aver2 - all_vec[j][i];
 			}
-			else if (n == 1) {
-				vec[i].push_back(-1);
-				continue;
-			}
-			if (n == 0)
-				empty_num++;
-			vec[i].push_back(n);
 		}
 	}
-	if (empty_num == 0) {
-		cout << 0 << "\n";
-		return 0;
-	}
 
-	virus_select(vec, 0, 0);
-
-	if (all_case != fail_case)
-		cout << result - 1 << endl;
-	else
-		cout << "-1\n";
+	cout << aver << " " << counting << endl;
 	system("pause");
 	return 0;
 }
