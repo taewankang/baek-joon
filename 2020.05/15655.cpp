746cb55712b3f4e29e672da9580d64af12a73c7b
taewankang <37101468+taewankang@users.noreply.github.com>
2020-05-27 18:22:42 +0900

    2020.05

---
+++ 2020.05/15655.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int N, M;
+vector<int> vec;
+bool check[10001] = { false, };
+void recursive(vector<int> &v, int idx, int depth){
+    if(M == depth){
+        for(int i = 0; i < M; i++)
+            cout << v[i] << " ";
+        cout << "\n";
+        return ;
+    }
+
+    for(int i = idx; i < vec.size(); i++){
+        if(check[vec[i]]) continue;
+        check[vec[i]] = true;
+        v[depth] = vec[i];
+        recursive(v, i, depth + 1);
+        check[vec[i]] = false;
+    }
+}
+
+int main(void){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> N >> M;
+    int n;    
+
+    for(int i = 0; i < N; i++){
+        cin >> n;
+        vec.push_back(n);
+    }
+
+    sort(vec.begin(), vec.end());
+
+    vector<int> v(vec.size(), 0);
+    for(int i = 0; i <= vec.size() - M; i++) {
+        v[0] = vec[i];
+        check[vec[i]] = true;
+        recursive(v, i, 1);
+        check[vec[i]] = false;
+    }
+    return 0;
+}
